33c5a324e68c27b3e08b04f9509aff7a4e1d0f4d
MJH <matthew.heys@gowlingwlg.com>
2017-04-04 21:40:52 +0100

    Add data annotations

--- TotalRecall/Models/TRApplication.cs
+++ TotalRecall/Models/TRApplication.cs
@@ -8,16 +8,20 @@ namespace TotalRecall.Models
         public int TRApplicationId { get; set; }
 
         [Required][Display(Name = "Application Name")]
+        [MaxLength(100, ErrorMessage = "Max Application Name is 100 characters")]
         public string Name { get; set; }
 
         [Display(Name = "Application Description", Description ="Description of the Application")]
+        [MaxLength(1000, ErrorMessage = "Max Descriptions length is 1000 characters")]
         public string Description { get; set; }
 
-        [Display(Name = "Public Key", Description = "Used to view data, anyone with this key can read the database")]
+        [Display(Name = "Public Key", Description = "Used to view data, anyone with this key can read your database")]
         public Guid PublicKey { get; set; }
 
+        [Display(Name = "Update Key", Description = "Used along with the Public Key to add data to your database")]
         public Guid UpdateKey { get; set; }
 
+        [Display(Name = "Admin Key", Description = "This is your master key and allows you to do delete or download your database")]
         public Guid AdminKey { get; set; }
 
         public bool Public { get; set; }
