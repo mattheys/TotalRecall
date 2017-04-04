8514157cae392adf1202e087370602d5acccaff4
MJH <matthew.heys@gowlingwlg.com>
2017-04-04 21:40:30 +0100

    Add data annotations

--- TotalRecall/Models/DataItem.cs
+++ TotalRecall/Models/DataItem.cs
@@ -1,10 +1,18 @@
-﻿namespace TotalRecall.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TotalRecall.Models
 {
     public class DataItem
     {
+
         public int DataItemId { get; set; }
+
         public int DataId { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Maximum Property Name is 50 characters long")]
         public string PropertyName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Maximum Property Value is 50 characters long")]
         public string PropertyValue { get; set; }
     }
 
