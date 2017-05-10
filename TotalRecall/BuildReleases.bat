99c189e42ad51393fac14c56f399c29761b93a18
MJH <matthew.heys@gowlingwlg.com>
2017-05-10 11:56:17 +0100

    Add Build File

---
+++ TotalRecall/BuildReleases.bat
@@ -0,0 +1,5 @@
+dotnet publish -c Release -r ubuntu.16.04-x64
+dotnet publish -c Release -r ubuntu.16.10-x64
+dotnet publish -c Release -r win81-x64
+dotnet publish -c Release -r osx.10.10-x64
+pause
